52b70aaaca9bd30758656c6ee617c622a6b755be
John van der Loo <jvanderloo@gmail.com>
2020-12-04 08:44:14 +0000

    Updated to use Repl.it

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,11 @@
 module.exports = {
   plugins: ["react"],
   parser: "babel-eslint",
+  settings: {
+    react: {
+      version: "detect",
+    }
+  },
   rules: {
     "react/prop-types": "error"
   }
